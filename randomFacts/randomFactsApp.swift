bddee3e75ef229d35e64f8c7a20194f9b2058594
Christy <randomtimeelaspe@gmail.com>
2025-04-06 09:20:12 -0400

    Initial Commit

---
+++ randomFacts/randomFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomFactsApp.swift
+//  randomFacts
+//
+//  Created by Christy on 4/6/25.
+//
+
+import SwiftUI
+
+@main
+struct randomFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ randomFactsTests/randomFactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  randomFactsTests.swift
+//  randomFactsTests
+//
+//  Created by Christy on 4/6/25.
+//
+
+import Testing
+@testable import randomFacts
+
+struct randomFactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
